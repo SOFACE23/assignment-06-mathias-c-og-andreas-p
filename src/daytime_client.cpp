72914bbfb9bd19d12bf956c549c231e0c98ba506
AndreasGBP <114382655+AndreasGBP@users.noreply.github.com>
2023-03-07 13:36:25 +0100

    WIP 1

--- src/daytime_client.cpp
+++ src/daytime_client.cpp
@@ -14,33 +14,33 @@
 
 using boost::asio::ip::tcp;
 
-int main(int argc, char* argv[])
+int main(int argc, char* argv[]) 
 {
   try
   {
-    if (argc != 2)
+    if (argc != 2) //Check the number of arguments
     {
-      std::cerr << "Usage: client <host>" << std::endl;
+      std::cerr << "Usage: client <host>" << std::endl; //Report the error
       return 1;
     }
 
-    boost::asio::io_context io_context;
+    boost::asio::io_context io_context; // All I/O objects are required to have an associated io_context object
 
-    tcp::resolver resolver(io_context);
+    tcp::resolver resolver(io_context); //Create a resolver object
     tcp::resolver::results_type endpoints =
-      resolver.resolve(argv[1], "daytime");
+      resolver.resolve(argv[1], "daytime"); //Resolve the host name to an IP address
 
     tcp::socket socket(io_context);
-    boost::asio::connect(socket, endpoints);
+    boost::asio::connect(socket, endpoints); //Connect to the server
 
-    while(true)
+    while(true) //Read the data from the socket
     {
-      boost::array<char, 128> buf;
-      boost::system::error_code error;
+      boost::array<char, 128> buf; //Buffer to hold the data
+      boost::system::error_code error; //Boost::system::error_code is a type used to report errors
 
       size_t len = socket.read_some(boost::asio::buffer(buf), error);
 
-      if (error == boost::asio::error::eof)
+      if (error == boost::asio::error::eof) //Check if the connection was closed cleanly by the peer
         break; // Connection closed cleanly by peer.
       else if (error)
         throw boost::system::system_error(error); // Some other error.
@@ -48,7 +48,7 @@ int main(int argc, char* argv[])
       std::cout.write(buf.data(), len);
     }
   }
-  catch (std::exception& e)
+  catch (std::exception& e) //Handle any exceptions that may have been thrown
   {
     std::cerr << e.what() << std::endl;
   }
--- src/daytime_server.cpp
+++ src/daytime_server.cpp
@@ -15,7 +15,7 @@
 
 using boost::asio::ip::tcp;
 
-std::string make_daytime_string()
+std::string make_daytime_string() //Create a string containing the current date and time
 {
   using namespace std; // For time_t, time and ctime;
   time_t now = time(0);
@@ -26,24 +26,24 @@ int main()
 {
   try
   {
-    boost::asio::io_context io_context;
+    boost::asio::io_context io_context; // All I/O objects are required to have an associated io_context object
 
-    tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), 13));
+    tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), 13)); //Create an acceptor object and open the acceptor
 
     while (true)
     {
-      tcp::socket socket(io_context);
-      acceptor.accept(socket);
+      tcp::socket socket(io_context); //Create a socket object
+      acceptor.accept(socket); //Wait for a connection
 
-      std::string message = make_daytime_string();
+      std::string message = make_daytime_string(); //Create the message to be sent back to the client
 
-      boost::system::error_code ignored_error;
-      boost::asio::write(socket, boost::asio::buffer(message), ignored_error);
+      boost::system::error_code ignored_error; //Boost::system::error_code is a type used to report errors
+      boost::asio::write(socket, boost::asio::buffer(message), ignored_error); //Write the message to the socket
     }
   }
-  catch (std::exception &e)
+  catch (std::exception &e) //Handle any exceptions that may have been thrown
   {
-    std::cerr << e.what() << std::endl;
+    std::cerr << e.what() << std::endl; //Report the error
   }
 
   return 0;
